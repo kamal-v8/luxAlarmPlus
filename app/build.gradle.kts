e31589320237eb1d1b522090b591f2eea65dd79b
kamal <kamal.v3011f@gmail.com>
2026-07-02 14:34:09 +0530

    release: v2.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.dsalmun.luxalarm"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10
-        versionName = "2.2.2"
+        versionCode = 11
+        versionName = "2.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
